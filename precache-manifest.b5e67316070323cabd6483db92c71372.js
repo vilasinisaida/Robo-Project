9de31fe5b30e5b65b66f7d0ac69c5c9fb816c71c
unknown <vilasini.modem@gmail.com>
2020-03-16 14:58:16 +0100

    Updates

---
+++ precache-manifest.b5e67316070323cabd6483db92c71372.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca6bec49801b8e84535c4be7c3183f7",
+    "url": "/Robo-Project/index.html"
+  },
+  {
+    "revision": "d8f621414b6b74fe9d22",
+    "url": "/Robo-Project/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "250a2b8aa52dcbbb1d01",
+    "url": "/Robo-Project/static/css/main.528b04cd.chunk.css"
+  },
+  {
+    "revision": "d8f621414b6b74fe9d22",
+    "url": "/Robo-Project/static/js/2.9dbeed3d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Robo-Project/static/js/2.9dbeed3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250a2b8aa52dcbbb1d01",
+    "url": "/Robo-Project/static/js/main.bad60648.chunk.js"
+  },
+  {
+    "revision": "445c24442e7ec2d67469",
+    "url": "/Robo-Project/static/js/runtime-main.30af266e.js"
+  }
+]);
